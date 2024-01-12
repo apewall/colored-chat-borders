b7e2cfa8722ecb741b5d8cdaab67da8087e9eba1
Apewall <1915182+apewall@users.noreply.github.com>
2024-01-11 22:47:20 -0600

    Fix syntax in settings.js

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -4,14 +4,14 @@ export let enableChatBorders: boolean;
 
 export const registerSettings = function() {
     game.settings.register("colored-chat-borders", "EnableChatBorders", {
-		name: "Enable colored chat borders",
-		hint: "Enable colored chat borders for all chat messages",
-        scope: 'world',
+        name: "Enable colored chat borders",
+        hint: "Enable colored chat borders for all chat messages",
+        scope: "world",
         config: true,
         type: Boolean,
-		default: true,
+        default: true,
         onChange: debouncedReload
-  });
+    });
 }
 
 export function getSettings() {

96863dfad87a73c67ebc380992b9112e9e371b74
Apewall <1915182+apewall@users.noreply.github.com>
2024-01-21 01:03:44 -0600

    Add colored bar to chat cards

--- src/colored-chat-borders.ts
+++ src/colored-chat-borders.ts
@@ -1,20 +1,24 @@
-import { registerSettings, getSettings, enableChatBorders } from './module/settings';
+import { registerSettings, getSettings, enableChatBorders, enableChatBars } from './module/settings';
 
 Hooks.once('init', async function() {
-    registerSettings();
-    getSettings();
-    console.log("colored-chat-borders | Initializing colored-chat-borders")
+  registerSettings();
+  getSettings();
+  console.log("colored-chat-borders | Initializing colored-chat-borders")
 });
 
 Hooks.on("renderChatMessage", (_message: ChatMessage, html: JQuery, messageData: any) => {
-    if (enableChatBorders) {
-        let authorColor: string = "black";
-        if (messageData.author.color) {
-            authorColor = messageData.author.color;
-        } else {                     
-            authorColor = messageData?.user.color;
-        }                            
-        html[0].style.borderColor = authorColor;
-        messageData.borderColor = authorColor;
-    }
+  let authorColor: string = "black";
+  if (messageData.author.color) {
+    authorColor = messageData.author.color;
+  }
+
+  if (enableChatBorders) {
+    html[0].style.borderColor = authorColor;
+  }
+
+  if (enableChatBars) {
+    let chatMessageHeader = html[0].children[0] as HTMLElement;
+    chatMessageHeader.style.borderBottom = `10px Solid ${authorColor}`;
+    chatMessageHeader.style.marginBottom = "5px";
+  }
 });
--- src/module/settings.ts
+++ src/module/settings.ts
@@ -1,27 +1,40 @@
 const debouncedReload = foundry.utils.debounce(() => window.location.reload(), 100);
 
 export let enableChatBorders: boolean;
+export let enableChatBars: boolean;
 
 export const registerSettings = function() {
-    game.settings.register("colored-chat-borders", "EnableChatBorders", {
-        name: "Enable colored chat borders",
-        hint: "Enable colored chat borders for all chat messages",
-        scope: "world",
-        config: true,
-        type: Boolean,
-        default: true,
-        onChange: debouncedReload
-    });
+  game.settings.register("colored-chat-borders", "EnableChatBorders", {
+    name: "Enable colored chat borders",
+    hint: "Enable colored chat borders for all chat messages",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: true,
+    onChange: debouncedReload
+  });
+
+  game.settings.register("colored-chat-borders", "EnableChatBars", {
+    name: "Enable colored chat bars",
+    hint: "Enable colored chat bars for all chat messages",
+    scope: "world",
+    config: true,
+    type: Boolean,
+    default: true,
+    onChange: debouncedReload
+  });
 }
 
 export function getSettings() {
-    enableChatBorders = Boolean(game.settings.get("colored-chat-borders", "EnableChatBorders"));
+  enableChatBorders = Boolean(game.settings.get("colored-chat-borders", "EnableChatBorders"));
+  enableChatBars = Boolean(game.settings.get("colored-chat-borders", "EnableChatBars"));
 }
 
 declare global {
-    namespace ClientSettings {
-        interface Values {
-            'colored-chat-borders.enableChatBorders': boolean;
-        }
+  namespace ClientSettings {
+    interface Values {
+      'colored-chat-borders.enableChatBorders': boolean;
+      'colored-chat-borders.EnableChatBars': boolean;
     }
+  }
 }
